95e2632825eaedf1e40f53d0ec09230defb7ce13
Maria may Roberts <mariamayr96@gmail.com>
2019-10-24 06:27:46 -0700

    readme

--- README.md
+++ README.md
@@ -1,23 +1,34 @@
 # liri-node-app
 
-### Overview
+### OVERVIEW
 
-In this assignment, you will make LIRI. LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
 
-### Before You Begin
+# TECHNOLOGIES/API's USE
 
-1. LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+* Spotify API
+* Node.js
+* DotEnv Node package
+* BandsInTown API
+* Moment.js Node package
+* OMDB (Open Movie Database) API
+* Request Node Package
+* Node-Spotify-API Node package
+ 
+# COMMAND LINE
 
-2. Make a new GitHub repository called liri-node-app and clone it to your computer.
+`concert-this`
 
-3. To retrieve the data that will power this app, you'll need to send requests using the `axios` package to the Bands in Town, Spotify and OMDB APIs. You'll find these Node packages crucial for your assignment.
+* This will search the Bands in Town Artist Events API , for an artist and render the following information about each event to the terminal:
 
-   * [Node-Spotify-API](npm install --save node-spotify-api)
+     * Name of the venue
 
-   * [Axios](https://www.npmjs.com/package/axios)
+     * Venue location
 
-     * You'll use Axios to grab data from the [OMDB API](http://www.omdbapi.com) and the [Bands In Town API](http://www.artists.bandsintown.com/bandsintown-api)
+     * Date of the Event (use moment to format this as "MM/DD/YYYY")
 
-   * [Moment](https://www.npmjs.com/package/moment)
+# Here's some of my screenshot for referrence for command line
 
-   * [DotEnv](https://www.npmjs.com/package/dotenv)
+![]()
+
+
--- liri.js
+++ liri.js
@@ -1,60 +1,61 @@
 require("dotenv").config();
-
+//Load exports from keys.js
 var keys = require("./keys.js");
+//Required libraries
 var moment = require("moment");
 var Spotify = require("node-spotify-api");
 var axios = require("axios");
 var spotify = new Spotify(keys.spotify);
 var fs = require("fs")
 
-
- function concertThis(artist) {
-        var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-        axios.get(queryUrl).then(
-            function(response) {
-                if(response.data[0].venue != undefined) {
-                    console.log("Venue: " + response.data[0].venue.name);
-                    console.log("Location: " + response.data[0].venue.city);
-                    var eventDateTime = moment(response.data[0].datetime);
-                    console.log("Date & Time: " + eventDateTime.format("dddd, MM do YYYY"));
-                }
-                else {
-                    console.log("Results not found!");
-                }
+//Create a function concert-this to get the information of the artist.
+function concertThis(artist) {
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get(queryUrl).then(
+        function (response) {
+            if (response.data[0].venue != undefined) {
+                //It will console.log the response that back like the venue name, venue location and the date. 
+                console.log("Venue: " + response.data[0].venue.name);
+                console.log("Location: " + response.data[0].venue.city);
+                var eventDateTime = moment(response.data[0].datetime);
+                console.log("Date & Time: " + eventDateTime.format("dddd, MM do YYYY"));
             }
-        )
-        .catch(function(error) {
+            else {
+                console.log("Results not found!");
+            }
+        }
+    )
+        .catch(function (error) {
             console.log(error);
         });
-   }
-   
-//============================================
+}
 
-     function spotifyThisSong(song) {
+//Create a function spotify-this-song 
+function spotifyThisSong(song) {
+    //Spotify API request (if an object is returned, output the first search result's artists, song, preview_url an album)
+    spotify.search({ type: "track", query: song })
+        .then(function (response) {
+            if (response.tracks.total === 0) {
+                errorConditionForSpotify();
+            }
+            else {
+                console.log("Artist: " + response.tracks.items[0].artists[0].name);
+                console.log("Track: " + response.tracks.items[0].name);
+                console.log("Preview URL: " + response.tracks.items[0].preview_url);
+                console.log("Album: " + response.tracks.items[0].album.album);
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+            console.log("Results not found. Showing result for 'The sign' by Ace of Base");
+        });
+}
 
-    spotify.search({type: "track", query: song}) 
-           .then(function(response) {
-               if(response.tracks.total === 0) {
-                   errorConditionForSpotify();
-               }
-               else {
-                 console.log("Artist: " + response.tracks.items[0].artists[0].name);
-                 console.log("Track: " + response.tracks.items[0].name);
-                 console.log("Preview URL: " + response.tracks.items[0].preview_url);
-                 console.log("Album: " + response.tracks.items[0].album.album);
-               }
-           })
-           .catch(function(error) {
-               console.log(error);
-               console.log("Results not found. Showing result for 'The sign' by Ace of Base");
-           });
-    }     
-
-    function errorConditionForSpotify() {
-        spotify
+function errorConditionForSpotify() {
+    spotify
         .search({ type: 'track', query: 'The Sign' })
-        .then(function(response) {
-            for (var i=0;i < response.tracks.items.length; i++) {
+        .then(function (response) {
+            for (var i = 0; i < response.tracks.items.length; i++) {
                 if (response.tracks.items[i].artists[0].name === "Ace of Base") {
                     console.log("Artist: " + response.tracks.items[i].artists[0].name);
                     console.log("Track: " + response.tracks.items[i].name);
@@ -63,48 +64,48 @@ var fs = require("fs")
                     i = response.tracks.items.length;
                 }
             }
-        }).catch(function (error) {  
+        }).catch(function (error) {
             console.log(error);
             console.log("No Results found. ");
-      });
-    }
-
-      function movieThis(movie) {
-    
-
-    axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&tomatoes=tue&apikey=trilogy")
-
-    .then(function(response) {
-
-        if (response.data.Title != undefined) {
-            console.log("Title: " + response.data.Title);
-            console.log("Year: " + response.data.Year);
-            console.log("imdbRating: " + response.data.imdRating);
-            console.log("Rotten Tomatoes Rating: " + response.data.tomatoRating);
-            console.log("Country: " + response.data.Country);
-            console.log("Language: " + response.data.Language);
-            console.log("Plot: " + response.data.Plot);
-            console.log("Actors: " + response.data.Actors);
-        }
-        else {
-            movieThis("Mr.Nobody");
-        }
-    })
-    .catch(function(error) {
-        console.log(error);
-        console.log("Results not found!")
-    });
+        });
 }
 
-        function doWhatItSays() {
-    fs.readFile("random.txt", "utf8", function(error, data) {
-        
+function movieThis(movie) {
+
+    //Http GET request
+    axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&tomatoes=tue&apikey=trilogy")
+
+        .then(function (response) {
+
+            if (response.data.Title != undefined) {
+                console.log("Title: " + response.data.Title);
+                console.log("Year: " + response.data.Year);
+                console.log("imdbRating: " + response.data.imdbRating);
+                console.log("Rotten Tomatoes Rating: " + response.data.rottenTomatoesRating);
+                console.log("Country: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors);
+            }
+            else {
+                movieThis("Mr.Nobody");
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+            console.log("Results not found!")
+        });
+}
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
         var dataArr = data.split(",");
 
-         spotifyThisSong(dataArr[1])
-         if(error) {
-             return console.log(error);
-         }
+        spotifyThisSong(dataArr[1])
+        if (error) {
+            return console.log(error);
+        }
     });
 }
 
@@ -118,19 +119,19 @@ fs.appendFile('log.txt', command + ",", function (err) {
 
 //Using switch command
 switch (command) {
-       case "concert-this":
-           concertThis(searchTerm);
-           break;
+    case "concert-this":
+        concertThis(searchTerm);
+        break;
 
-        case "spotify-this-song":
-            spotifyThisSong(searchTerm);
-            break;
+    case "spotify-this-song":
+        spotifyThisSong(searchTerm);
+        break;
 
-        case "movie-this":
-            movieThis(searchTerm);
-            break;
+    case "movie-this":
+        movieThis(searchTerm);
+        break;
 
-        case "do-what-it-says":
-            doWhatItSays(searchTerm);
-            break;
+    case "do-what-it-says":
+        doWhatItSays(searchTerm);
+        break;
 }
