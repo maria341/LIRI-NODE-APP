f6ce5dea26eeac2f6fe03d93d3cc55e975e550f8
Maria may Roberts <mariamayr96@gmail.com>
2019-10-24 23:06:56 -0700

    Updated File

--- liri.js
+++ liri.js
@@ -1,23 +1,26 @@
 require("dotenv").config();
 //Load exports from keys.js
-var keys = require("./keys.js");
+let keys = require("./keys.js");
 //Required libraries
-var moment = require("moment");
-var Spotify = require("node-spotify-api");
-var axios = require("axios");
-var spotify = new Spotify(keys.spotify);
-var fs = require("fs")
+let moment = require("moment");
+let Spotify = require("node-spotify-api");
+let axios = require("axios");
+let spotify = new Spotify(keys.spotify);
+let fs = require("fs")
 
 //Create a function concert-this to get the information of the artist.
-function concertThis(artist) {
-    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+function searchThis(artist) {
+    // Run the axios.get function...
+    // The axios.get function takes in a URL and returns a promise (just like $.ajax)
+    let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     axios.get(queryUrl).then(
         function (response) {
+
             if (response.data[0].venue != undefined) {
                 //It will console.log the response that back like the venue name, venue location and the date. 
                 console.log("Venue: " + response.data[0].venue.name);
                 console.log("Location: " + response.data[0].venue.city);
-                var eventDateTime = moment(response.data[0].datetime);
+                let eventDateTime = moment(response.data[0].datetime);
                 console.log("Date & Time: " + eventDateTime.format("dddd, MM do YYYY"));
             }
             else {
@@ -31,7 +34,7 @@ function concertThis(artist) {
 }
 
 //Create a function spotify-this-song 
-function spotifyThisSong(song) {
+function spotifyThis(song) {
     //Spotify API request (if an object is returned, output the first search result's artists, song, preview_url an album)
     spotify.search({ type: "track", query: song })
         .then(function (response) {
@@ -51,32 +54,14 @@ function spotifyThisSong(song) {
         });
 }
 
-function errorConditionForSpotify() {
-    spotify
-        .search({ type: 'track', query: 'The Sign' })
-        .then(function (response) {
-            for (var i = 0; i < response.tracks.items.length; i++) {
-                if (response.tracks.items[i].artists[0].name === "Ace of Base") {
-                    console.log("Artist: " + response.tracks.items[i].artists[0].name);
-                    console.log("Track: " + response.tracks.items[i].name);
-                    console.log("Preview URL: " + response.tracks.items[i].preview_url);
-                    console.log("Album: " + response.tracks.items[i].album.name);
-                    i = response.tracks.items.length;
-                }
-            }
-        }).catch(function (error) {
-            console.log(error);
-            console.log("No Results found. ");
-        });
-}
-
-function movieThis(movie) {
-
-    //Http GET request
+//Create a movie function
+function movieInfo(movie) {
+    // Run the axios.get function...
+    // The axios.get function takes in a URL and returns a promise (just like $.ajax)
     axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&tomatoes=tue&apikey=trilogy")
 
         .then(function (response) {
-
+            //The request was made and the server response with status code
             if (response.data.Title != undefined) {
                 console.log("Title: " + response.data.Title);
                 console.log("Year: " + response.data.Year);
@@ -88,7 +73,7 @@ function movieThis(movie) {
                 console.log("Actors: " + response.data.Actors);
             }
             else {
-                movieThis("Mr.Nobody");
+                movieInfo("Mr.Nobody");
             }
         })
         .catch(function (error) {
@@ -100,9 +85,9 @@ function movieThis(movie) {
 function doWhatItSays() {
     fs.readFile("random.txt", "utf8", function (error, data) {
 
-        var dataArr = data.split(",");
+        let dataArr = data.split(",");
 
-        spotifyThisSong(dataArr[1])
+        spotifyThis(dataArr[1])
         if (error) {
             return console.log(error);
         }
@@ -111,7 +96,7 @@ function doWhatItSays() {
 
 //pull in required variables
 let command = process.argv[2]
-let searchTerm = process.argv[3]
+let liriString = process.argv[3]
 
 fs.appendFile('log.txt', command + ",", function (err) {
     if (err) throw err;
@@ -120,18 +105,18 @@ fs.appendFile('log.txt', command + ",", function (err) {
 //Using switch command
 switch (command) {
     case "concert-this":
-        concertThis(searchTerm);
+        searchThis(liriString);
         break;
 
     case "spotify-this-song":
-        spotifyThisSong(searchTerm);
+        spotifyThis(liriString);
         break;
 
     case "movie-this":
-        movieThis(searchTerm);
+        movieInfo(liriString);
         break;
 
     case "do-what-it-says":
-        doWhatItSays(searchTerm);
+        doWhatItSays(liriString);
         break;
 };
